822539ace6ad1522a80d5a0725886c59af9c71cf
keerthisdpt1 <78008893+keerthisdpt1@users.noreply.github.com>
2021-06-12 16:48:48 +0800

    Update CalcTest.java

--- src/test/java/com/edureka/TestApp/CalcTest.java
+++ src/test/java/com/edureka/TestApp/CalcTest.java
@@ -36,7 +36,7 @@ public class CalcTest
 	    String myAppName = prop.getProperty("app_name");
 	    String myURL = "http://" + myIP + ":" + myPort + "/" + myAppName;
 	    //String myURL = "http://130.211.229.175:9090/calculator/";
-	     String myURL = "http://34.134.180.1:9090/calculator/";
+	    // String myURL = "http://34.134.180.1:9090/calculator/";
 	    System.out.println("Opening " + myURL);
 	    
 	    FirefoxOptions options = new FirefoxOptions();
