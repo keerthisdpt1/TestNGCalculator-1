a1514e061a4ae486ced0dbfaddfcbc811d500581
keerthisdpt1 <78008893+keerthisdpt1@users.noreply.github.com>
2021-06-12 16:55:50 +0800

    Update ColorTest.java

--- src/test/java/com/edureka/TestApp/ColorTest.java
+++ src/test/java/com/edureka/TestApp/ColorTest.java
@@ -23,7 +23,7 @@ public class ColorTest
 	    String myIP = prop.getProperty("public_ip");
 	    String myPort = prop.getProperty("tomcat_port");
 	    String myAppName = prop.getProperty("app_name");
-	    String myURL = "http://" + myIP + ":" + myPort + "/" + myAppName;
+	   // String myURL = "http://" + myIP + ":" + myPort + "/" + myAppName;
 	    //String myURL = "http://130.211.229.175:9090/calculator/";
 	    String myURL = "http://34.134.180.1:9090/calculator/";
 	    FirefoxOptions options = new FirefoxOptions();
